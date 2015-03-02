ebfda5a5112f7f6302e7e79241a1df8e9ba79971
Exequiel Pierotto <exequiel.pierotto@gmail.com>
2015-03-02 11:46:18 +0100

    first commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'sensu-manage'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures sensu-manage'
+long_description 'Installs/Configures sensu-manage'
+version          '0.1.0'
+
